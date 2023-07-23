46266f9db25d01bcdc33da02610820c39f5b2dd8
itsLeo <107639845+itsJhnL@users.noreply.github.com>
2023-07-23 10:07:26 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Made by JanggoDev</h1>
+    </div>
+  );
+}
+
+export default App;
