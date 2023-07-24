5197daae3cdbd68ecd4fe3774492346118d58748
itsLeo <107639845+itsJhnL@users.noreply.github.com>
2023-07-24 15:37:53 +0800

    Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import './App.css';
+// import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>Made by JanggoDev</h1>
+      {/* Header */}
+      <Header />
+      {/* App Body */}
+      {/* Sidebar */}
+      {/* Feed */}
+      {/* Widgets */}
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,36 @@
+import React from "react";
+import SearchIcon from "@mui/icons-material/Search";
+import LinkedIn from "./images/linkedin.png";
+import "./Header.css";
+import HeaderOption from "./HeaderOption";
+import HomeIcon from "@mui/icons-material/Home";
+import PeopleIcon from "@mui/icons-material/People";
+import WorkIcon from "@mui/icons-material/Work";
+import MessageIcon from "@mui/icons-material/Message";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+
+function Header() {
+  return (
+    <div>
+      <div className="header">
+        <div className="header__left">
+          <img src={LinkedIn} alt="" />
+
+          <div className="header__search">
+            <SearchIcon />
+            <input type="text" placeholder="Search" />
+          </div>
+        </div>
+        <div className="header__right">
+          <HeaderOption Icon={HomeIcon} title="Home" />
+          <HeaderOption Icon={PeopleIcon} title="My Network" />
+          <HeaderOption Icon={WorkIcon} title="Jobs" />
+          <HeaderOption Icon={MessageIcon} title="Messaging" />
+          <HeaderOption Icon={NotificationsIcon} title="Notifications" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
