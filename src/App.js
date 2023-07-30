60b0e0cf0e5a20c83b3e92e1bfe3ac59d38f275b
itsLeo <107639845+itsJhnL@users.noreply.github.com>
2023-07-30 23:55:15 +0800

    ff

--- src/App.js
+++ src/App.js
@@ -3,20 +3,25 @@ import "./App.css";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
 import Feed from "./components/Feed";
+import { useSelector } from "react-redux";
+import { selectUser } from "./features/userSlice";
+import Login from "./components/Login";
 
 function App() {
+  const user = useSelector(selectUser);
+
   return (
     <div className="App">
-      {/* Header */}
       <Header />
-      {/* App Body */}
-      <div className="app__body">
-        {/* Sidebar */}
-        <Sidebar />
-        {/* Feed */}
-        <Feed />
-        {/* Widgets */}
-      </div>
+      {/* {!user ? (
+        <Login />
+      ) : ( */}
+        <div className="app__body">
+          <Sidebar />
+          <Feed />
+          {/* Widgets */}
+        </div>
+      {/* )} */}
     </div>
   );
 }
---
+++ src/components/Login.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Login.css";
+
+function Login() {
+  return (
+    <div className="login">
+      <form>
+        <input type="text" placeholder="Full Name" />
+      </form>
+    </div>
+  );
+}
+
+export default Login;
